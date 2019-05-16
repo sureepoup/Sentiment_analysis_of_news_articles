47d14c2468559ae1488cf20762708d783c2742d9
corvushospes <corvushospes@gmail.com>
2019-05-16 18:27:16 +0300

    Some fixes

--- preprocessing_tools.py
+++ preprocessing_tools.py
@@ -71,3 +71,36 @@ def lemmatization(text):
     words_lem_without_stopwords = [i for i in words_lem if not i in stopword_ru] # [6]
 
     return words_lem_without_stopwords
+
+
+def stop_words_remove(text):
+    '''
+    очистка текста от стоп-слов
+    
+    на выходе - очищенный от стоп-слов текст
+    '''
+    # [0]
+    if not isinstance(text, str):
+        text = str(text)
+    
+    # [1]
+    tokens = list(tokenize(text))
+    words = [_.text for _ in tokens]
+
+    words_lem = []
+    for w in words:
+        if w[0] == '-': # [2]
+            w = w[1:]
+        if len(w)>1: # [3]
+            if w in cache: # [4]
+                words_lem.append(cache[w])
+            else: # [5]
+                temp_cach = cache[w] = morph.parse(w)[0].normal_form
+                words_lem.append(temp_cach)
+
+    words_lem_without_stopwords = [i for i in words_lem if not i in stopword_ru] # [6]
+    
+    words_lem_without_stopwords_string = str(words_lem_without_stopwords)
+
+    return words_lem_without_stopwords_string
+    
